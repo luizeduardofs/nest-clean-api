a400218e22238097ae13bf12eed19965feb03df9
luizeduardofs <edu.dev.py@gmail.com>
2025-04-19 00:15:01 -0300

    first commit

---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,19 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { ZodError, ZodSchema } from 'zod';
+import { fromZodError } from 'zod-validation-error';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown) {
+    try {
+      this.schema.parse(value);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException(fromZodError(error));
+      }
+      throw new BadRequestException('Validation failed');
+    }
+    return value;
+  }
+}
