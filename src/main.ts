f124b7a0d49697e090f310354a84c5f46f65442b
luizeduardofs <edu.dev.py@gmail.com>
2025-04-19 01:07:21 -0300

    Generate JWT token started

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Env } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    // logger: false,
+    logger: false,
   });
-  await app.listen(process.env.PORT ?? 3333);
+
+  const configService: ConfigService<Env, true> = app.get(ConfigService);
+  const port = configService.get('PORT', { infer: true });
+
+  await app.listen(port);
 }
 bootstrap();
